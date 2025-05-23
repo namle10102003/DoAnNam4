883febd49b3565c8640205c5b93467b650c72721
Lezt Waper <leztwaper@gmail.com>
2025-05-23 20:13:00 +0700

    feat: Make Mocking Database

---
+++ main/java/com/example/myapplication/Domain/Plan.java
@@ -0,0 +1,45 @@
+package com.example.myapplication.Domain;
+
+import androidx.annotation.NonNull;
+
+public class Plan {
+    private int id;
+    private int userId;
+    private String name;
+
+    public Plan(int id, int userId, String name) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
